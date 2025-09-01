e99f74621f91fade3ecc08cb75637a3997539b82
SANTHAN-KUMAR <kiranmayeekarnala@gmail.com>
2025-09-01 23:34:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-alert("new page loaded ra chosoosh");
+/*
+
+1st question's solution : Create variables using let, const, and var for a user’s name (string), age (number), isAdmin (boolean), hobbies (array), and profile (object). Log each type with typeof.
+
+let name="kunju";
+const age=18;
+var isAdmin=false;
+const profile = {"Role" : "user", "place" : "nellore", "phno": 1234};
+let hobbies=["eating","sleeping"];
+
+console.log("Type of isAdmin:", typeof isAdmin);
+document.body.innerHTML = "message from : "+name;
+
+*/
+
+
+/* 2nd Question solution : 
+
+let str1 = "42";
+let str2 = "42abc";
+
+
+let num1 = Number(str1);
+let num2 = parseInt(str1);
+let num3 = +str1;
+
+let num4 = Number(str2);
+let num5 = parseInt(str2);
+let num6 = +str2;
+
+document.body.innerHTML =
+  `<b>For "42":</b><br>` +
+  `Number("42") = ${num1}, type: ${typeof num1}, isNaN: ${isNaN(num1)}, Number.isNaN: ${Number.isNaN(num1)}<br>` +
+  `parseInt("42") = ${num2}, type: ${typeof num2}, isNaN: ${isNaN(num2)}, Number.isNaN: ${Number.isNaN(num2)}<br>` +
+  `+"42" = ${num3}, type: ${typeof num3}, isNaN: ${isNaN(num3)}, Number.isNaN: ${Number.isNaN(num3)}<br><br>` +
+
+  `<b>For "42abc":</b><br>` +
+  `Number("42abc") = ${num4}, type: ${typeof num4}, isNaN: ${isNaN(num4)}, Number.isNaN: ${Number.isNaN(num4)}<br>` +
+  `parseInt("42abc") = ${num5}, type: ${typeof num5}, isNaN: ${isNaN(num5)}, Number.isNaN: ${Number.isNaN(num5)}<br>` +
+  `+"42abc" = ${num6}, type: ${typeof num6}, isNaN: ${isNaN(num6)}, Number.isNaN: ${Number.isNaN(num6)}<br>`;
+*/
+
+// document.body.innerHTML= (null == undefined);
+
+/* 4th Question solution :
+
+const arr=[1,2,3];
+const obj={
+  "kunj_id":123, "kunj_ph":3373
+};
+document.body.innerHTML=Array.isArray(arr) + '<br>' +
+Array.isArray(obj);;
+
+*/
